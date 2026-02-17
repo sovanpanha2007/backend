97aef198f87939d892ac90dd1338393e415d7338
sovanpanha2007 <sovanpanha2007@gmail.com>
2026-02-17 23:02:53 +0700

    guessingGame

---
+++ rustc/guessing_game/src/main.rs
@@ -0,0 +1,17 @@
+// use std::io; // bring input/output library from standard library
+
+
+fn main() {
+    // println!("Guess a number !");
+    // println!("Please input your guess");
+
+    // let mut guess = String::new();
+    // let mut guess_number = 0;
+    // std::io::stdin().read_line(&mut guess).expect("Failed to read line");
+    // println!("You guessed: {guess}");
+
+    let x =10;
+    let y =1;
+
+    println!("x + y ={} {} ", x + y, x);
+}
