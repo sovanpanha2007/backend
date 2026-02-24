388f36790f535c104a3af9e7e5acb56e63e6e127
sovanpanha2007 <sovanpanha2007@gmail.com>
2026-02-24 12:42:13 +0700

    Completed guessing game code

--- rustc/guessing_game/src/main.rs
+++ rustc/guessing_game/src/main.rs
@@ -5,26 +5,34 @@ use std::cmp::Ordering;
 
 fn main() {
     println!("Guess a number !");
-    println!("Please input your guess");
 
-    let mut guess = String::new();
     let secret = rand::thread_rng().gen_range(1..=100); // thread.rng(): give particular random number genertor depend on gen_range(start..=end)t
-    io::stdin().read_line(&mut guess).expect("Failed to read line");
     //shadowing the same varible's name
     // convert from String to Integer by using 
     // trim() : elimate any whitespace at the beginning and end 
-    // parse() : convertor between variable type's
+    // parse() : enum type : return OK and Err stages : convertor between variable type's
     // ": u32" : tell Rust we'll annotate the variable type, since u32 is an unsinged, 32 bit integer.
-    let guess: u32 = guess.trim().parse().expect("Please type a number");
 
-    println!("The secret number: {secret}");
-    println!("You guessed: {guess}");
-    
-
-    match guess.cmp(&secret) {
-        Ordering::Less => println!("Too small!"),
-        Ordering::Greater => println!("Too big!"),
-        Ordering::Equal => println!("You win!"),
+    println!("The secret number: {secret}");    
+    loop {
+         println!("Please input your guess");
+         let mut guess = String::new();
+         io::stdin().read_line(&mut guess).expect("Failed to read line");
+         let guess: u32 = guess.trim().parse() {
+             Ok(number) => number,
+             Err(_) => continue, // if the user input is not a number, it will continue to the next iteration of the loop
+         };
+            println!("You guessed: {guess}");
+         match guess.cmp(&secret) {
+           Ordering::Less => println!("Too small!"),
+           Ordering::Greater => println!("Too big!"),
+           Ordering::Equal =>
+           { 
+               println!("You win!");
+               break;
+           }
+         
+       }
     }
     // let x = 10;
     // match x {
